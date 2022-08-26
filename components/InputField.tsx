94412ea6c3be97f44b31253f5311ad2c4ebb315d
Devarat Patel <devaratpatel1@gmail.com>
2022-08-26 16:58:43 -0400

    fixed UI blur bug on input field

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props {
@@ -6,10 +6,26 @@ interface Props {
   setTodo: React.Dispatch<React.SetStateAction<string>>;
   handleAdd: (event: React.FormEvent) => void;
 }
+
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className='input' onSubmit={handleAdd}>
-      <input type='input' placeholder='Enter a task' value={todo} onChange={(event) => setTodo(event.target.value)} className='input__box'></input>
+    <form
+      className='input'
+      onSubmit={(event) => {
+        handleAdd(event);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type='input'
+        ref={inputRef}
+        placeholder='Enter a task'
+        value={todo}
+        onChange={(event) => setTodo(event.target.value)}
+        className='input__box'
+      ></input>
       <button className='input_submit' type='submit'>
         Go
       </button>
